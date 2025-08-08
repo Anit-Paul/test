e292ef460ac2269ffc25d1ca6c6cd78e702daece
Anit Paul <anit42655@gmail.com>
2025-08-09 00:18:00 +0530

    initialize backend with app.js and basic structure

---
+++ backend/routers/index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const router=express.Router()
+const controller=require("../controllers/index")
+
+router.get("/data",controller.getData)
+router.post("/data",controller.postData)
+router.put("/data",updateData)
+router.delete("/data",controller.deleteData)
+
+module.exports=router
